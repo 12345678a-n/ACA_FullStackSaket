eda56c6e480a616db74da953031564a8963ead1f
12345678a-n <saketrajput2005@gmail.com>
2026-05-27 19:14:58 +0530

    pika

--- Assignemnt1/index.js
+++ Assignemnt1/index.js
@@ -52,33 +52,33 @@ const rl = readline.createInterface({
 });
  
 //--------------CLI code to read from command line arguments------------------//
-// let argv = process.argv.slice(2);
-// if(argv.length < 4) {
-//     console.log("Error : Insufficient data");
-//     process.exit(1);
-// } 
-// let student = new Student(argv[0], argv.slice(1).map(Number));
-// console.log(`Name: ${student.name}\nScores: ${student.scores}\nAverage: ${student.average.toFixed(1)}\nLetter Grade: ${student.letterGrade}\nHigh/Low: ${student.summary().max}/${student.summary().min}\n`);
-// student.average >= 60 ? console.log("PASS\n") : console.log("FAIL\n");
-// console.log(`Remark : ${getRemark(student.letterGrade)}\n`);
-// let [score1, score2,...rem] = student.scores;
-// console.log(`First score : ${score1}\nSecond score : ${score2}\nRemaining scores : ${rem}\n`);
-// process.exit(0);
+let argv = process.argv.slice(2);
+if(argv.length < 4) {
+    console.log("Error : Insufficient data");
+    process.exit(1);
+} 
+let student = new Student(argv[0], argv.slice(1).map(Number));
+console.log(`Name: ${student.name}\nScores: ${student.scores}\nAverage: ${student.average.toFixed(1)}\nLetter Grade: ${student.letterGrade}\nHigh/Low: ${student.summary().max}/${student.summary().min}\n`);
+student.average >= 60 ? console.log("PASS\n") : console.log("FAIL\n");
+console.log(`Remark : ${getRemark(student.letterGrade)}\n`);
+let [score1, score2,...rem] = student.scores;
+console.log(`First score : ${score1}\nSecond score : ${score2}\nRemaining scores : ${rem}\n`);
+process.exit(0);
  
 //--------------Uncomment the below code and comment the above code to read from file instead of command line arguments------------------//
-console.log("Reading file......\n");
-const fs = require('fs');
-const data = fs.readFileSync('data.txt', 'utf-8');
+// console.log("Reading file......\n");
+// const fs = require('fs');
+// const data = fs.readFileSync('data.txt', 'utf-8');
 
-for(let line of data.split('\r\n')){
-  let arr = line.split(' ');
+// for(let line of data.split('\r\n')){
+//   let arr = line.split(' ');
   
-  let student = new Student(arr[0], arr.slice(1).map(Number));
-  console.log(`Name: ${student.name}\nScores: ${student.scores}\nAverage: ${student.average.toFixed(1)}\nLetter Grade: ${student.letterGrade}\nHigh/Low: ${student.summary().max}/${student.summary().min}\n`);
-  student.average >= 60 ? console.log("PASS\n") : console.log("FAIL\n");
-  console.log(`Remark : ${getRemark(student.letterGrade)}\n`);
-  let [score1, score2,...rem] = student.scores;
-  console.log(`First score : ${score1}\nSecond score : ${score2}\nRemaining scores : ${rem}\n`);
-  console.log("--------------------------------------------------\n");
-}
-process.exit(0);
+//   let student = new Student(arr[0], arr.slice(1).map(Number));
+//   console.log(`Name: ${student.name}\nScores: ${student.scores}\nAverage: ${student.average.toFixed(1)}\nLetter Grade: ${student.letterGrade}\nHigh/Low: ${student.summary().max}/${student.summary().min}\n`);
+//   student.average >= 60 ? console.log("PASS\n") : console.log("FAIL\n");
+//   console.log(`Remark : ${getRemark(student.letterGrade)}\n`);
+//   let [score1, score2,...rem] = student.scores;
+//   console.log(`First score : ${score1}\nSecond score : ${score2}\nRemaining scores : ${rem}\n`);
+//   console.log("--------------------------------------------------\n");
+// }
+// process.exit(0);
